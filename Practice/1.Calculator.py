5a68da2768e388d5c8575f32327c35199dc4cea4
iibbrraahhiimm588-coder <iibbrraahhiimm588@gmail.com>
2025-12-28 12:16:49 +0500

    Add files via upload

---
+++ Practice/1.Calculator.py
@@ -0,0 +1,11 @@
+# First one is I will create 2 variables
+
+a = 5
+b = 10
+
+# Second one is i will print this variables using calculator
+
+print(a, '+', b, '=', (a + b))
+print(a, '-', b, '=', (a - b))
+print(a, 'x', b, '=', (a * b))
+print(a, '/', b, '=', (a / b))
